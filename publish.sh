69ef70d897e2ebbf0959f951ce69eb8b52a194ad
harryross <h.ross@outlook.com>
2015-10-10 12:42:40 +1300

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # DEFAULT="circle_ci"
 # PROFILE=${AWS_PROFILE:-$DEFAULT}
-BUCKET=hross.io
+BUCKET=hross.io.s3-website-ap-southeast-2.amazonaws.com
 DIR=dist/
 aws  s3  sync $DIR s3://$BUCKET/
