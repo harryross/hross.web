c85e05008519f94f6b116860b81b6f3f2f94c6e3
harryross <h.ross@outlook.com>
2015-10-10 13:42:24 +1300

    Update publish.sh

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -8,7 +8,7 @@ var Home = React.createClass({
     render: function() {
         return (
             <div>
-                <h1>This is my website</h1>
+                <h1>Hi James</h1>
                 <p>This is currently just a playground for me</p>
                 <Link to="about" className="ui primary button">Learn More</Link>
             </div>
