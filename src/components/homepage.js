7dc0e7d968c4614661a7deb85b8e4cbccae0e4cf
harryross <h.ross@outlook.com>
2015-10-12 10:55:04 +1300

    Update homepage.js

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -8,7 +8,7 @@ var Home = React.createClass({
     render: function() {
         return (
             <div className="main-content">
-                <h1>Hi James, what do you think?</h1>
+                <h1>Hi James (except James Espie), what do you think?</h1>
                 <p>This is currently just a playground for me</p>
                 <Link to="about" className="ui primary button">Learn More</Link>
             </div>
